bd5e5b3503c70cae71d4068fa461853f21991d9d
Ruby E Stoios (DEECA) <ruby.stoios@delwp.vic.gov.au>
2023-06-13 15:50:59 +1000

    new plots

--- LF_charts.R
+++ LF_charts.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+
 flood_sample_data <- fetch_query(
   "SELECT id_site, site_name, waterbody, id_project, sdate, yr, rank, scientific_name, common_name, fork_length_mm, length_mm, weight_g
 	FROM projects.v_floods_sample_data",
@@ -59,5 +60,19 @@ flood_sample_data <- flood_sample_data[flood_sample_data$scientific_name %in% sp
   theme_bw() +
   facet_wrap( ~ scientific_name, scales = "free")
 )
+
+#Ruby new plot
+#just carp and cod, hypoxia rank= H and M 
+#density and histogram 
+
+lenfreqnew1<- flood_sample_data_try|>filter(hypoxia_rank %in% c("H", "M"), scientific_name %in% c("Cyprinus carpio", "Maccullochella peelii"))|>
+  ggplot(aes(x=length_mm))+
+  scale_fill_manual(aesthetics = 'fill', values = c("#4FC3F7", "#E57373"), name="Legend", labels=c('Before', 'After'))+
+  geom_histogram(aes(y=..density.., fill=before_after), binwidth=20, boundary=0, closed="left",color='black', position="dodge", alpha=0.5)+
+  geom_density(aes(fill=before_after), alpha=0.7, lwd=0.8)+
+  theme_bw()+
+  facet_wrap(~scientific_name, scales= "free")
+
+lenfreqnew1
 
 aaedb_disconnect()
