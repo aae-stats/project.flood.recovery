81f5403ebab154f1816557dbd1c48945baf69414
Ruby E Stoios (DEECA) <ruby.stoios@delwp.vic.gov.au>
2023-06-15 17:06:57 +1000

    updated plots

--- LF_charts.R
+++ LF_charts.R
@@ -98,4 +98,98 @@ lenfreqnew3<- flood_sample_data_try|>filter(hypoxia_rank %in% c("H", "L"), scien
 
 lenfreqnew3
 
+lenfreqcarp<- flood_sample_data_try|>filter(hypoxia_rank %in% c("H", "M"), 
+                                            scientific_name %in% c("Cyprinus carpio"), 
+                                            length_mm<150)|>
+  ggplot(aes(x=length_mm))+
+  scale_fill_manual(aesthetics = 'fill', values = c("#4FC3F7", "#E57373"), name="Legend", labels=c('Before', 'After'))+
+  geom_histogram(aes(y=..count.., fill=before_after), binwidth=20, boundary=0, closed="left",color='black', position="dodge", alpha=0.5)+
+  #geom_density(aes(fill=before_after), alpha=0.7, lwd=0.8)+
+  labs(x = "Total Length (mm)", y = "Number of Fish")+
+  theme_bw()+
+  facet_wrap(~scientific_name, scales= "free")
+
+lenfreqcarp
+
+lenfreqcod<- flood_sample_data_try|>filter(hypoxia_rank %in% c("H", "M"), 
+                                           scientific_name %in% c("Maccullochella peelii"), 
+                                           length_mm<=130)|>
+  ggplot(aes(x=length_mm))+
+  scale_fill_manual(aesthetics = 'fill', values = c("#4FC3F7", "#E57373"), name="Legend", labels=c('Before', 'After'))+
+  geom_histogram(aes(y=..count.., fill=before_after), binwidth=20, boundary=0, closed="left",color='black', position="dodge", alpha=0.5)+
+  #geom_density(aes(fill=before_after), alpha=0.7, lwd=0.8)+
+  labs(x = "Total Length (mm)", y = "Number of Fish")+
+  theme_bw()+
+  facet_wrap(~scientific_name, scales= "free")+
+  annotate(geom="text", x=15, y=12, label="(d)",size=4)
+
+
+lenfreqcod
+
+lenfreqblackfish<- flood_sample_data_try|>filter(hypoxia_rank %in% c("L"), 
+                                                 scientific_name %in% c("Gadopsis marmoratus"), 
+                                                 length_mm<80)|>
+  ggplot(aes(x=length_mm))+
+  scale_fill_manual(aesthetics = 'fill', values = c("#4FC3F7", "#E57373"), name="Legend", labels=c('Before', 'After'))+
+  geom_histogram(aes(y=..count.., fill=before_after), binwidth=20, boundary=0, closed="left",color='black', position="dodge", alpha=0.5)+
+  #geom_density(aes(fill=before_after), alpha=0.7, lwd=0.8)+
+  labs(x = "Total Length (mm)", y = "Number of Fish")+
+  theme_bw()+
+  facet_wrap(~scientific_name, scales= "free")+
+  annotate(geom="text", x=10, y=35, label="(c)",size=4)
+
+
+lenfreqblackfish
+
+lenfreqtroutcod<- flood_sample_data_try|>filter(hypoxia_rank %in% c("L"), 
+                                                scientific_name %in% c("Maccullochella macquariensis"), 
+                                                length_mm<130)|>
+  ggplot(aes(x=length_mm))+
+  scale_fill_manual(aesthetics = 'fill', values = c("#4FC3F7", "#E57373"), name="Legend", labels=c('Before', 'After'))+
+  geom_histogram(aes(y=..count.., fill=before_after), binwidth=20, boundary=0, closed="left",color='black', position="dodge", alpha=0.5)+
+  #geom_density(aes(fill=before_after), alpha=0.7, lwd=0.8)+
+  theme_bw()+
+  facet_wrap(~scientific_name, scales= "free")+
+  annotate(geom="text", x=13, y=35, label="(b)",size=4)
+
+
+lenfreqtroutcod
+
+lenfreqmacca<- flood_sample_data_try|>filter(hypoxia_rank %in% c("L"), 
+                                             scientific_name %in% c("Macquaria australasica"), 
+                                             length_mm<100)|>
+  ggplot(aes(x=length_mm))+
+  scale_fill_manual(aesthetics = 'fill', values = c("#4FC3F7", "#E57373"), name="Legend", labels=c('Before', 'After'))+
+  geom_histogram(aes(y=..count.., fill=before_after), binwidth=20, boundary=0, closed="left",color='black', position="dodge", alpha=0.5)+
+  #geom_density(aes(fill=before_after), alpha=0.7, lwd=0.8)+
+  theme_bw()+
+  facet_wrap(~scientific_name, scales= "free")+
+  annotate(geom="text", x=10, y=12, label="(a)",size=4)
+
+lenfreqmacca
+
+bottom<-textbox_grob("Total Length (mm)", vjust = 1, x= 0.9)
+
+left<- textbox_grob("Number of Fish", orientation=c("left-rotated"), y=0.9)
+
+
+# Create user-defined function, which extracts legends from ggplots
+extract_legend <- function(my_ggp) {
+  step1 <- ggplot_gtable(ggplot_build(my_ggp))
+  step2 <- which(sapply(step1$grobs, function(x) x$name) == "guide-box")
+  step3 <- step1$grobs[[step2]]
+  return(step3)
+}
+
+# Apply user-defined function to extract legend
+shared_legend <- extract_legend(lenfreqblackfish)
+
+grid.arrange(arrangeGrob(lenfreqmacca+theme(legend.position = "none", strip.text.x = element_text( size=12), axis.title.x = element_blank(), axis.title.y = element_blank()),
+                         lenfreqtroutcod+theme(legend.position="none", strip.text.x = element_text( size=12), axis.title.x= element_blank(), axis.title.y = element_blank()),
+                         lenfreqblackfish +theme(legend.position = "none", strip.text.x = element_text( size=12), axis.title.x= element_blank(), axis.title.y = element_blank()),
+                         lenfreqcod+theme(legend.position="none", strip.text.x = element_text( size=12), axis.title.x= element_blank(), axis.title.y = element_blank()),
+                         bottom=bottom, left=left, right=shared_legend,
+                         nrow=2, ncol=2))
+
+
 aaedb_disconnect()
